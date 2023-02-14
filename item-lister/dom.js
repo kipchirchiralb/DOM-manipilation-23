e95a94418155c5a79330d48006174b0f0e9938c5
Zablon Oigo <imjunior254@gmail.com>
2023-02-14 14:22:51 +0300

    css properties

---
+++ item-lister/dom.js
@@ -0,0 +1,13 @@
+//console.dir(document)
+//check domain
+// console.log(document.domain);
+//check web URL
+//console.log(document.URL)
+//check for document title
+// console.log(document.title)
+// check for DocumentType
+// console.log(document.doctype)
+
+
+
+/* */ 
